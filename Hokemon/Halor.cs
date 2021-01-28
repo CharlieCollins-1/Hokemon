9877816a850ea127f40f50559bc2fdd5c05a7ba6
charlie <charlie@UPSTAIRSPC>
2021-01-28 10:13:33 +0000

    Changes

--- Hokemon/Halor.cs
+++ Hokemon/Halor.cs
@@ -6,14 +6,12 @@ namespace Hokemon
 {
     class Halor : Hokemon
     {
-        class Halor : Hokemon
-        {
-            private string team = "Halor";
+        private string team = "Halor";
 
-            public void about() // Using POLYMORPHISM to change method INHERITED from Hokemo parent class
-            {
-                Console.WriteLine("I am {0} part of the {1} team!!\n\n", Name, team);
-            }
+        public void about()
+        {
+            Console.WriteLine("I am {0} part of the {1} team!!\n\n", Name, team);
         }
     }
 }
+
